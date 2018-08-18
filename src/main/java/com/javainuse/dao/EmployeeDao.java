60b72a9d88a9daa8e608c88c485b3732dd7e9e5a
mk.java88 <mk.java88@gmail.com>
2018-08-17 21:01:07 -0400

    Formated file

--- src/main/java/com/javainuse/dao/EmployeeDao.java
+++ src/main/java/com/javainuse/dao/EmployeeDao.java
@@ -6,7 +6,10 @@ import com.javainuse.model.Employee;
 
 public interface EmployeeDao {
 	void insertEmployee(Employee cus);
+
 	void insertEmployees(List<Employee> employees);
+
 	List<Employee> getAllEmployees();
+
 	Employee getEmployeeById(String empId);
 }
